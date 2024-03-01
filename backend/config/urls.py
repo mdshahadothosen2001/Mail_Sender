045d420aff03562488404b3f7d6867c9beeeeac3
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-03-01 12:23:49 +0600

    chore: send mail view  modified

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("send/", include("send.urls")),
+    path(
+        route="admin/",
+        view=admin.site.urls,
+        name="admin",
+    ),
+    path(
+        route="send/",
+        view=include("send.urls"),
+        name="send",
+    ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
